c8bf37ba2b702257d53e053ef2874f83f95961bd
migfus20 <migfus20@gmail.com>
2023-04-28 15:36:21 +0800

    roles update

--- resources/js/views/users/roles/contents/ResponsiveCard.vue
+++ resources/js/views/users/roles/contents/ResponsiveCard.vue
@@ -1,57 +1,23 @@
 <template>
-  <div v-for="row in $role.content" :key="row.id" class="col-md-6 col-12">
+  <div v-for="row in $role.content" :key="row.id" class="col-md-4 col-12">
 
-
-    <div :class="`card mb-2`">
-      <div class="card-header">
-        <div>
-          <div class="row">
-            <div class="col-12">
-              <div class="card-tools float-right">
-                <strong>{{ row.count }}</strong> Users
-              </div>
-
-              <div :class="`d-flex justify-content-center align-items-center img-circle float-left mr-3 bg-${row.color}`"
-                style="width: 40px; height: 40px">
-                <i :class="`fas ${row.icon}`"></i>
-              </div>
-              <div> <strong> {{ row.name }} </strong> </div>
-            </div>
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex justify-content-between mb-2">
+          <h6 class="fw-normal">Total {{ row.count }} users</h6>
+          <ul class="list-unstyled d-flex align-items-center avatar-group mb-0">
+            <li v-for="row2 in row.top" class="avatar avatar-sm pull-up">
+              <img :src="row2.avatar" class="rounded-circle" style="width: 40px; height: 40px;">
+            </li>
+          </ul>
+        </div>
+        <div class="d-flex justify-content-between align-items-end">
+          <div class="role-heading">
+            <h4 class="mb-1">{{ row.name }}</h4>
           </div>
+          <a href="javascript:void(0);" class="text-muted"><i :class="`fas ${row.icon} text-${row.color}`"></i></a>
         </div>
       </div>
-
-      <div class="card-body table-responsive p-0">
-        <table class="table table-striped table-valign-middle">
-          <thead>
-            <tr>
-              <th>Username</th>
-              <th>Email</th>
-              <th>More</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="row2 in row.top" :key="row2.id">
-              <td>
-                <img :src="row2.avatar" alt="Product 1" class="img-circle img-size-32 mr-2">
-                {{ row2.username }}
-              </td>
-              <td>{{ FullNameConvert(row2.email) }}</td>
-              <td>
-                <a href="#" class="text-muted">
-                  <i class="fas fa-search"></i>
-                </a>
-              </td>
-            </tr>
-            <tr v-if="row.top">
-              <td>
-                <RouterLink :to="{ name: 'users-list' }">More Users</RouterLink>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-
     </div>
   </div>
 </template>
@@ -67,3 +33,14 @@ onMounted(() => {
   $role.RoleGetAPI()
 });
 </script>
+
+<style scoped>
+.avatar-group .avatar-sm {
+  margin-left: -0.75rem;
+}
+
+.avatar {
+  border: 5px solid #c9dfe2;
+  border-radius: 50px
+}
+</style>
