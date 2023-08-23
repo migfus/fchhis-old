5d5a2598a0f9461aff2ab87cc2f9a381a0dd0550
migfus20 <migfus20@gmail.com>
2023-08-23 14:49:59 +0800

    adding region & branch on users

---
+++ database/seeders/RegionSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RegionSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+      //
+    $data = [
+      [
+        'name' => 'Region X',
+      ]
+    ];
+
+    foreach($data as $row) {
+      \App\Models\Region::create($row);
+    }
+  }
+}
